11b50d3a157b80578006c64dba9502119d87920b
Sirapat903 <s6703052411368@email.kmutnb.ac.th>
2026-01-15 10:28:39 +0700

    add refs array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
-import { Calculator2 } from './calculator';
-import  Calendar from './class-components';
+
 
 import React from 'react'
-import { EventData2 } from './event-data';
+import RefsArray from './refs-array'
 
 function App() {
-return <EventData2/>
+return <RefsArray/>
   }
 
 
