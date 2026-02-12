14fe1cee463e5f9537959e4bc603ef951c9abf18
Sirapat903 <s6703052411368@email.kmutnb.ac.th>
2026-02-12 10:10:14 +0700

    Navlink

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
+import './App.css';
 import React from 'react'
-import { userContext } from './context';
-import Content from './context-content2';
-import Header2 from './context-header2';
+import Router2 from './router2';
 
 export default function App() {
-  let [user, setUser] = React.useState("Tom Jerry");
-  return (
-    <userContext.Provider value={[user, setUser]}>
-      <Header2/>  
-      <Content/>
-    </userContext.Provider>
-  );
+ return <Router2/>
 }
  
 
