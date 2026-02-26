d88092f372f0ed01af4b8360a6c6376a1478fd4b
Sirapat903 <s6703052411368@email.kmutnb.ac.th>
2026-02-26 10:30:11 +0700

    add
    form-select-file.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 import React from 'react'
-
-import FormText from './form-text';
+import FormSelectFile from './form-select-file';
 
 export default function App() {
- return <FormText/>
+ return <FormSelectFile/>
 }
  
 
